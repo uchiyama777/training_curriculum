05037a28a24bb5e53232fe19e486df6b36509dd1
uchiyama777 <ucchi_uchi@hotmail.co.jp>
2020-10-09 12:41:00 +0900

    issue6修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,9 +34,17 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
-      @week_days.push(days)
-    end
 
+      wday_num = Date.today.wday + x #を利用して添字となる数値を得る
+
+      #もしもwday_numが7以上であれば、7を引く
+      if wday_num >= 7 #条件式を記述
+        wday_num = wday_num - 7
+      end
+
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num]}
+      @week_days.push(days)
+      #この配列(week_day)の中にdaysを入れている。
+    end
   end
 end
